e8c0bb909851367678fbfa1e6ffa76a0d9cd859f
Simon Goodman <simon.goodman84@googlemail.com>
2021-12-07 07:03:55 +0000

    Day 7 Part 2

--- AdventOfCode2021/Day07/Day07.cs
+++ AdventOfCode2021/Day07/Day07.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode2021.Day07
     {
         public int DayNumber => 7;
         public string ValidatedPart1 => "353800";
-        public string ValidatedPart2 => "";
+        public string ValidatedPart2 => "98119739";
 
         private List<int> _crabPositions;
 
@@ -24,12 +24,23 @@ namespace AdventOfCode2021.Day07
 
         public string Part1()
         {
+            return GetResult(x => x);
+        }
+
+
+        public string Part2()
+        {
+            return GetResult(GetFuelForDistance);
+        }
+
+        private string GetResult(Func<int, long> getFuelForDistance)
+        {
             var minPosition = _crabPositions.Min();
             var maxPosition = _crabPositions.Max();
             var count = maxPosition - minPosition;
 
             var fuelForPositions = Enumerable.Range(minPosition, count)
-                .Select(pos => (Position: pos, Fuel: _crabPositions.Sum(cp => Math.Abs(pos - cp))))
+                .Select(pos => (Position: pos, Fuel: _crabPositions.Sum(cp => getFuelForDistance(Math.Abs(pos - cp)))))
                 .ToList();
 
             var minimalFuel = fuelForPositions
@@ -39,9 +50,20 @@ namespace AdventOfCode2021.Day07
             return minimalFuel.Fuel.ToString();
         }
 
-        public string Part2()
+        private Dictionary<int, long> _fuelForDistance = new Dictionary<int, long>();
+        private long GetFuelForDistance(int distance)
         {
-            return "";
+            if (!_fuelForDistance.ContainsKey(distance))
+            {
+                if (distance == 0)
+                {
+                    return 0;
+                }
+
+                _fuelForDistance[distance] = GetFuelForDistance(distance - 1) + distance;
+            }
+
+            return _fuelForDistance[distance];
         }
     }
 }
