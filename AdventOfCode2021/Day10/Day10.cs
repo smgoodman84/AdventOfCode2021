22cfd40c4772848a187d37aa2ff6ad832a284221
Simon Goodman <simon.goodman84@googlemail.com>
2021-12-10 07:38:09 +0000

    Day 10 Part 2

--- AdventOfCode2021/Day10/Day10.cs
+++ AdventOfCode2021/Day10/Day10.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode2021.Day10
     {
         public int DayNumber => 10;
         public string ValidatedPart1 => "268845";
-        public string ValidatedPart2 => "";
+        public string ValidatedPart2 => "4038824534";
 
         private List<NavigationParser> _lines;
 
@@ -29,7 +29,18 @@ namespace AdventOfCode2021.Day10
 
         public string Part2()
         {
-            return "";
+            var incompleteLines = _lines
+                .Where(l => l.GetSyntaxErrorScore() == 0)
+                .ToList();
+
+            var completionScores = incompleteLines
+                .Select(l => l.GetCompletionScore())
+                .OrderBy(score => score)
+                .ToArray();
+
+            var middle = (completionScores.Length - 1) / 2;
+
+            return completionScores[middle].ToString();
         }
 
         private class NavigationParser
@@ -74,6 +85,38 @@ namespace AdventOfCode2021.Day10
                 }
 
                 return 0;
+            }
+
+            public long GetCompletionScore()
+            {
+                var stack = new Stack<char>();
+
+                foreach (var c in _input)
+                {
+                    if (new[] { '(', '[', '{', '<' }.Contains(c))
+                    {
+                        stack.Push(c);
+                        continue;
+                    }
+
+                    stack.Pop();
+                }
+
+                long score = 0;
+
+                while (stack.TryPop(out var c))
+                {
+                    score *= 5;
+                    switch (c)
+                    {
+                        case '(': score += 1; break;
+                        case '[': score += 2; break;
+                        case '{': score += 3; break;
+                        case '<': score += 4; break;
+                    }
+                }
+
+                return score;
             }
         }
     }
